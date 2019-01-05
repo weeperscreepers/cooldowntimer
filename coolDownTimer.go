bec894a1725954d8ae310560f5206232a7530eb7
cburke <cburke@live.com>
2019-01-05 12:06:47 -0800

    Another extract from a different project

---
+++ coolDownTimer.go
@@ -0,0 +1,42 @@
+package utils
+
+import(
+//  "math"
+ "fmt"
+)
+
+type CooldownTimer struct{
+  TimeLeftMillis float64
+  Interval float64
+  GoneOff bool
+}
+
+func NewCooldownTimer(interval float64) *CooldownTimer {
+  return &CooldownTimer{interval, interval, false}
+}
+
+
+func (c *CooldownTimer) PhaseShift(delta float64){
+  (*c).TimeLeftMillis += delta;
+}
+
+func (c *CooldownTimer) CoolDown(coolDown float64){
+  interval := (*c).Interval
+  timeLeft := &((*c).TimeLeftMillis)
+  *timeLeft -= coolDown;
+  if(*timeLeft < -interval){
+    fmt.Println("uh-oh, we're badly behind", *timeLeft - interval)
+  }
+  for *timeLeft < 0.0{
+    *timeLeft += interval
+    (*c).GoneOff = true
+  }
+}
+
+func (c *CooldownTimer) CheckAndReset() bool{
+  if((*c).GoneOff){
+    (*c).GoneOff = false;
+    return true;
+  }
+  return false;
+}
---
+++ coolDownTimer_test.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+  "fmt"
+  "testing"
+)
+
+func TestSum(t *testing.T) {
+
+    fmt.Println("testing begins...")
+    cd100 := NewCooldownTimer(100)
+
+    fmt.Println("-------- 100 pre-cooldown", cd100)
+    cd100.CoolDown(100)
+    fmt.Println("-------- 100 after ------", cd100)
+
+    if cd100.CheckAndReset() {
+       t.Errorf("Timer went off and it shouldn't have...")
+    }
+
+    fmt.Println("-------- 100 pre-cooldown", cd100)
+    cd100.CoolDown(1)
+    fmt.Println("-------- 100 after ------", cd100)
+
+    if !cd100.CheckAndReset() {
+       t.Errorf("Timer didn't go off and it should have...")
+    }
+
+    if cd100.CheckAndReset() {
+       t.Errorf("Timer went off and it shouldn't have...")
+    }
+
+
+    cd2 := NewCooldownTimer(2);
+
+    fmt.Println("-------- 2 pre-cooldown", cd2)
+    cd2.CoolDown(3)
+    fmt.Println("-------- 2 after ------", cd2)
+
+    if !cd2.CheckAndReset() {
+       t.Errorf("Timer didn't go off and it should have...")
+    }
+
+    fmt.Println("-------- 2 pre-cooldown", cd2)
+    cd2.CoolDown(1)
+    fmt.Println("-------- 2 after ------", cd2)
+
+    if cd2.CheckAndReset() {
+        t.Errorf("Timer went off and it shouldn't have...")
+    }
+}
